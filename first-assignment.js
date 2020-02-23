52cb64fb822eb67d40b685484b9c7b96fba86d99
Jascha Franke <hello@jrfranke.com>
2020-02-23 16:44:08 +0100

    chunk data

--- first-assignment.js
+++ first-assignment.js
@@ -3,11 +3,12 @@ const http = require('http');
 const requestHandler = http.createServer((req, res) => {
   // console.log('hello');
   const url = req.url;
+  const method = req.method;
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>My first assignment</title></head>');
     res.write('<body><h1>Hi there. I wish you a beatiful Sunday.</h1>');
-    res.write('<form><input><button>Send</button></form>')
+    res.write('<form action="/create-user" method="POST"><input type="text" name="createAUser"><button>Send</button></form>')
     res.write('</body>');
     res.write('</html>');
     return res.end();
@@ -18,6 +19,13 @@ const requestHandler = http.createServer((req, res) => {
     res.write('<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>');
     res.write('</html>')
     return res.end();
+  }
+  if (url === '/' && method === 'POST') {
+    const body = [];
+    res.on('data', () => {
+      console.log(data);
+      body.push(data);
+    })
   }
 })
 
