3da5099f734c5f6d3b65584d55f040a9dff3601d
Jascha Franke <hello@jrfranke.com>
2020-02-23 16:48:17 +0100

    eventListener and redirection

--- first-assignment.js
+++ first-assignment.js
@@ -8,7 +8,7 @@ const requestHandler = http.createServer((req, res) => {
     res.write('<html>');
     res.write('<head><title>My first assignment</title></head>');
     res.write('<body><h1>Hi there. I wish you a beatiful Sunday.</h1>');
-    res.write('<form action="/create-user" method="POST"><input type="text" name="createAUser"><button>Send</button></form>')
+    res.write('<form action="/create-user" method="POST"><input placeholder="Create a user" type="text" name="user"><button>Send</button></form>')
     res.write('</body>');
     res.write('</html>');
     return res.end();
@@ -25,6 +25,14 @@ const requestHandler = http.createServer((req, res) => {
     res.on('data', () => {
       console.log(data);
       body.push(data);
+    })
+    return req.on('end', () => {
+      parsedBody = Buffer.concat(body).toString();
+      user = parsedBody.split('=')[1];
+      console.log(user);
+      res.statusCode = 302;
+      res.setHeader('Location', '/users');
+      res.end();
     })
   }
 })
