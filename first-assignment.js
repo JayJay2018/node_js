8a062a07557570fea33d4e330c06a612454a8926
Jascha Franke <hello@jrfranke.com>
2020-02-23 17:10:18 +0100

    added content type and refactoring

--- first-assignment.js
+++ first-assignment.js
@@ -5,6 +5,7 @@ const requestHandler = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
   if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My first assignment</title></head>');
     res.write('<body><h1>Hi there. I wish you a beatiful Sunday.</h1>');
@@ -15,6 +16,7 @@ const requestHandler = http.createServer((req, res) => {
     return res.end();
   }
   if (url === '/users') {
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Some users</title></head>');
     res.write('<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>');
@@ -29,10 +31,15 @@ const requestHandler = http.createServer((req, res) => {
     })
     return req.on('end', () => {
       parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
       user = parsedBody.split('=')[1];
       console.log(user);
       res.statusCode = 302;
       res.setHeader('Location', '/users')
+      res.write('<html>');
+      res.write('<head><title>Created user successfully</title></head>');
+      res.write('<body><h1>Creation successful</h1></body>');
+      res.write('</html>');
       return res.end();
     })
   }
