2ccbb4deb943cbda66f342cc34f69baf8d4c409b
Jascha Franke <hello@jrfranke.com>
2020-02-28 21:42:20 +0100

    add routes

--- express-example/app.js
+++ express-example/app.js
@@ -4,13 +4,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Hello from middleware 1');
+app.use('/', (req, res, next) => {
+  console.log('Hello from middleware 1 that always runs');
   next();
 })
 
-app.use((req, res, next) => {
+app.use('/add-item', (req, res, next) => {
   console.log('Hello from another middleware')
+  return res.status(200).send('<p>Add a item you bloody bastard.</p>')
+})
+
+app.use('/', (req, res, next) => {
+  console.log('Hello from middleware 1 that always runs');
+  res.status(200).send('<h2>Hi from middleware 1</h2>')
 })
 
 const server = http.createServer(app);
