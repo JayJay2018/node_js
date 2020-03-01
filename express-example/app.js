6da6bf1225bdc377277243d9372e987989d65406
Jascha Franke <hello@jrfranke.com>
2020-03-01 20:46:24 +0100

    body parser

--- express-example/app.js
+++ express-example/app.js
@@ -2,18 +2,26 @@ const http = require('http');
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/', (req, res, next) => {
   console.log('Hello from middleware 1 that always runs');
   next();
 })
 
-app.use('/add-item', (req, res, next) => {
+app.get('/add-product', (req, res, next) => {
   console.log('Hello from another middleware')
-  return res.status(200).send('<p>Add a item you bloody bastard.</p>')
+  return res.status(200).send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add a item you bloody bastard.</button></form>')
 })
 
+app.post('/product', (req,res) => {
+  console.log(req.body);
+  return res.redirect('/');
+})
 app.use('/', (req, res, next) => {
   console.log('Hello from middleware 1 that always runs');
   res.status(200).send('<h2>Hi from middleware 1</h2>')
