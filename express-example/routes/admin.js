2cd67cc426952a1a0c3b521fd7a8c4cb796addee
Jascha Franke <hello@jrfranke.com>
2020-03-01 22:22:24 +0100

    dynamic filterhing path

--- express-example/routes/admin.js
+++ express-example/routes/admin.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
   console.log('Hello from another middleware')
-  return res.status(200).send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add a item you bloody bastard.</button></form>')
+  return res.status(200).send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add a item you bloody bastard.</button></form>')
 })
 
-router.post('/product', (req,res) => {
+router.post('/add-product', (req,res) => {
   console.log(req.body);
   return res.redirect('/');
 })
